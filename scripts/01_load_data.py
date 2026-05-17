f9a04c3699933fe22d8d003e56648be078382b5a
Volodymyr Yusyp <volodymyr.yusyp@gmail.com>
2026-05-17 03:28:21 +0300

    add arhitech

---
+++ scripts/01_load_data.py
@@ -0,0 +1,23 @@
+import os
+import ast
+import pandas as pd
+from pymongo import MongoClient
+from tqdm import tqdm
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.environ['MONGO_URI']
+DB_NAME = 'spotify'
+CSV_PATH = './dataset.csv'
+BATCH_SIZE = 1000
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+
+db['tracks_raw'].drop()
+
+df = pd.read_csv(CSV_PATH)
+print(f'Заватажено {len(df)} треків...')
+
+df['explicit'] = df['explicit'].astype(bool)
